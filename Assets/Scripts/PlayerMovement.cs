7dbd338138ea2d619e45c5127dca822346ddebcb
unknown <kaionedaniels285@gmail.com>
2025-03-07 14:56:19 -0700

    fixing player ship stuffs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 4f;
     public float maxSpeed = 4f;
-
     private Rigidbody2D rigidBody;
 
-
-    private Vector2 m_MoveVal;
 
 
     // Start is called before the first frame update
@@ -19,14 +16,19 @@ public class PlayerMovement : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
         MoveCharacter();
     }
 
     private void MoveCharacter()
-    {
+    {   /*
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
@@ -35,9 +37,17 @@ public class PlayerMovement : MonoBehaviour
         velocity.x = horizontalInput * moveSpeed;
         velocity.y = verticalInput * moveSpeed;
 
-
-        // Assign back the changed velocity
-        rigidBody.velocity = Vector2.ClampMagnitude(4*velocity, maxSpeed);
+        rigidBody.velocity = Vector2.ClampMagnitude(100*velocity, maxSpeed);
+        */
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (input.magnitude > 0)
+        {
+            rigidBody.velocity = input.normalized * maxSpeed;
+        }
+        else
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
     }
 
 }
