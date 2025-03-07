9f31dbae541ebf29de4f0cc72e4158f5fca81b59
unknown <kaionedaniels285@gmail.com>
2025-03-07 16:19:28 -0700

    scrolling background

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,18 +28,10 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void MoveCharacter()
-    {   /*
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        var velocity = rigidBody.velocity;
-
-        velocity.x = horizontalInput * moveSpeed;
-        velocity.y = verticalInput * moveSpeed;
-
-        rigidBody.velocity = Vector2.ClampMagnitude(100*velocity, maxSpeed);
-        */
+    {   
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // Instantaneous Acceleration
         if (input.magnitude > 0)
         {
             rigidBody.velocity = input.normalized * maxSpeed;
---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackground : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+
+    public float startY = 20.4f; //use 20.4f for 2048x2048 images
+    public float resetY = 20.4f; //use 20.4f for 2048x2048 images
+    private Vector2 offset;
+    private Transform backgroundImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        backgroundImage = GetComponent<Transform>();
+        //startY = backgroundImage.position.y;
+        //resetY = startY - distanceTravel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        backgroundImage.position += Vector3.down * scrollSpeed * Time.deltaTime;
+
+        Debug.Log(backgroundImage.position.y);
+        if (backgroundImage.position.y <= -resetY) {
+            backgroundImage.position = new Vector3(transform.position.x, startY, transform.position.z);
+        }
+    }
+}
