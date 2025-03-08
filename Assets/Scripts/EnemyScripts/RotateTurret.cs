7b8365fea7f23c8ed90d37e178b21f0f7923d035
unknown <kaionedaniels285@gmail.com>
2025-03-07 20:40:14 -0700

    turret can now face player and speed of turret turn can be capped

--- Assets/Scripts/EnemyScripts/RotateTurret.cs
+++ Assets/Scripts/EnemyScripts/RotateTurret.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class RotateTurret : MonoBehaviour
 {
+    public float rotationSpeedDeg = 1f;
+    private GameObject player;
+    private float degOffset = -90f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        facePlayer();
+    }
+
+    void facePlayer()
+    {
+        Vector3 turretPosition = transform.position;
+        Vector3 playerPosition = player.transform.position;
+        float turretCurrentRotationZ = transform.eulerAngles.z;
+
+        // Determining angle of rotation
+        float turretTargetRotationDegrees = Mathf.Rad2Deg * Mathf.Atan2( ( turretPosition.y - playerPosition.y) , ( turretPosition.x - playerPosition.x) ) + degOffset;
+        Debug.Log(turretTargetRotationDegrees);
+        float newAngle = Mathf.MoveTowardsAngle(turretCurrentRotationZ, turretTargetRotationDegrees, rotationSpeedDeg * Time.deltaTime);
+        Debug.Log(newAngle);
+        transform.rotation = Quaternion.Euler( 0, 0, newAngle );
     }
 }
