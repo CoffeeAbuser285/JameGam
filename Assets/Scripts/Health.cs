3625b1f2be91ee91683c2b3846a9bfe30e83cd8e
unknown <kaionedaniels285@gmail.com>
2025-03-08 18:48:40 -0700

    player and enemy becomes more red as they get hurt

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Linq;
 
 public class Health : MonoBehaviour
 {
@@ -8,10 +10,20 @@ public class Health : MonoBehaviour
     private float currentHealth = 3f;
     private bool isDead;
     private GameObject canvas;
+    private SpriteRenderer[] spriteRenderers;
+    private TilemapRenderer[] tilemapRenderers;
 
     public void Start()
     {
         canvas = GameObject.FindWithTag( "Canvas" );
+
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        tilemapRenderers = GetComponentsInChildren<TilemapRenderer>();
+
+        spriteRenderers = spriteRenderers.Where(sr => sr.tag != "NoColorChange").ToArray();
+        //tilemapRenderers = tilemapRenderers.Where(tr => tr.tag != "NoColorChange").ToArray();
+        
+        
         currentHealth = initialHealth;
         isDead = false;
     }
@@ -28,10 +40,43 @@ public class Health : MonoBehaviour
             Destroy( gameObject );
         }
     }
+
+    private IEnumerator UpdateColor()
+    {
+        // Set all renderers to white
+        foreach (SpriteRenderer sr in spriteRenderers)
+        {
+            sr.color = Color.white;
+        }
+        foreach (TilemapRenderer tr in tilemapRenderers)
+        {
+            tr.GetComponent<Tilemap>().color = Color.white;
+        }
+
+        // Wait for 0.1 seconds
+        yield return new WaitForSeconds(0.1f);
+
+
+        float healthPercent = currentHealth / initialHealth;
+        Color newColor = new Color(1f, Mathf.Lerp(0.5f, 1f, healthPercent), Mathf.Lerp(0.5f, 1f, healthPercent));
+
+        // Apply the color change to all sprites
+        foreach (SpriteRenderer sr in spriteRenderers)
+        {
+            sr.color = newColor;
+        }
+        
+        // Change color of all TilemapRenderers
+        foreach (TilemapRenderer tr in tilemapRenderers)
+        {
+            tr.GetComponent<Tilemap>().color = newColor;
+        }
+    }
     
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        StartCoroutine(UpdateColor());
         checkIfDead();
     }
 
