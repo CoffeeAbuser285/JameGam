e7b0f36a720d75f09f203560e02c34732e662339
unknown <kaionedaniels285@gmail.com>
2025-03-09 00:17:44 -0700

    fixed rolling texture bug

--- Assets/Scripts/GeneralScripts/ScrollBackground1.cs
+++ Assets/Scripts/GeneralScripts/ScrollBackground1.cs
@@ -2,29 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScrollBackground : MonoBehaviour
+public class ScrollBackground1 : MonoBehaviour
 {
     public float scrollSpeed = 0.5f;
 
     public float startY = 20.4f; //use 20.4f for 2048x2048 images
     public float resetY = 20.4f; //use 20.4f for 2048x2048 images
+    private Material bgMaterial;
     private Vector2 offset;
-    private Transform backgroundImage;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        backgroundImage = GetComponent<Transform>();
+        bgMaterial = GetComponent<Renderer>().material;
+        offset = bgMaterial.mainTextureOffset;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        backgroundImage.position += Vector3.down * scrollSpeed * Time.deltaTime;
-
-        if (backgroundImage.position.y <= -resetY) {
-            backgroundImage.position = new Vector3(transform.position.x, startY, transform.position.z);
-        }
+        offset.y += scrollSpeed * Time.deltaTime;
+        bgMaterial.mainTextureOffset = offset;
     }
 }
