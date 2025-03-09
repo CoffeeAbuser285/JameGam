ea69e9de61cf5256fa4f476f3d763a76ec0f27f0
unknown <kaionedaniels285@gmail.com>
2025-03-09 13:24:40 -0700

    incrementing layer of spawning enemies to avoid overlap

--- Assets/Scripts/ManagerScripts/SpawnEnemies.cs
+++ Assets/Scripts/ManagerScripts/SpawnEnemies.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class SpawnEnemies : MonoBehaviour
 {
     public GameObject[] enemyPrefabs; // The enemy prefab
     public Transform[] spawnPoints; 
+    public string[] enemyLayers;
     public float spawnInterval = 20f; // Initial spawn interval
     public float spawnIntervalDecrease = 0.05f; // How much to decrease spawn interval over time
     public float minimumSpawnInterval = 5f; // The minimum spawn interval
     private float gameTime = 0f; // To track how long the game has been running
+    private int numCount = 0;
 
     void Start()
     {
@@ -37,7 +40,7 @@ public class SpawnEnemies : MonoBehaviour
         }
     }
 
-    void SpawnEnemy()
+    private void SpawnEnemy()
     {
         // Choose a random spawn point from the array
         Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
@@ -55,6 +58,52 @@ public class SpawnEnemies : MonoBehaviour
             Debug.Log("Setting Health");
             Debug.Log(health);
             enemy.GetComponent<Health>().setInitialHealth( health );
+        }
+
+        // Pick a random layer name from the array of layers
+        string randomLayer = enemyLayers[ numCount ];
+
+        // Increment Layer
+        IncrementCounter();
+
+        // Optional: If the enemy has child objects, you can also assign the layer to them
+        SetLayerRecursively(enemy.transform, randomLayer);
+    }
+
+    private void SetLayerRecursively(Transform objTransform, string layer)
+    {
+        // Check if the object has a TilemapRenderer component
+        TilemapRenderer tilemapRenderer = objTransform.GetComponent<TilemapRenderer>();
+        if (tilemapRenderer != null)
+        {
+            tilemapRenderer.sortingLayerName = layer;
+            tilemapRenderer.sortingOrder = 0;
+        }
+
+        // Check if the object has a SpriteRenderer component
+        SpriteRenderer spriteRenderer = objTransform.GetComponent<SpriteRenderer>();
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sortingLayerName = layer;
+            spriteRenderer.sortingOrder = 1;
+        }
+
+        // Recursively call this method for each child
+        foreach (Transform child in objTransform)
+        {
+            SetLayerRecursively(child, layer);
+        }
+    }
+
+    private void IncrementCounter()
+    {
+        if ( numCount >= enemyLayers.Length )
+        {
+            numCount = 0;
+        }
+        else
+        {
+            numCount += 1;
         }
     }
 }
